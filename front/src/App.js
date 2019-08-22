3f74516523a6f90df379468fa6283fa6e8ab2ee7
aguiot-- <aguiot--@student.42.fr>
2019-08-22 17:16:56 +0200

    Start /messages

--- front/src/App.js
+++ front/src/App.js
@@ -6,24 +6,33 @@ import Login  from './components/login/Login';
 import Signup from './components/signup/Signup';
 import Browse from './components/browse/Browse';
 import TestQL from './components/TestQL';
+import Messages from './components/messages/Messages';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
 
-library.add(fab, faCheckSquare, faCoffee, tamere)
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+library.add(faCheckSquare, faCoffee, faStar);
+
+const client = new ApolloClient({uri: 'http://localhost:4000/api'});
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Start} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/browse" component={Browse} />
-        <Route path="/testql" component={TestQL} />
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Start} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/browse" component={Browse} />
+          <Route path="/testql" component={TestQL} />
+          <Route path="/messages" component={Messages} />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
