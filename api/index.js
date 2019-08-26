06afc22e76be297c5ca73f8d9347fcf64e4eadd6
aguiot-- <aguiot--@student.42.fr>
2019-08-26 21:35:20 +0200

    Move to Apollo Server

--- api/index.js
+++ api/index.js
@@ -1,24 +1,21 @@
 import express from 'express';
 import cors    from 'cors';
-import exprgql from 'express-graphql';
 import schema  from './graphql/schema.js';
 import routes  from './routes.js';
-import { v1 as neo4j } from 'neo4j-driver';
+import { v1 as neo4j }  from 'neo4j-driver';
+import { ApolloServer } from 'apollo-server-express';
 
 const driver = neo4j.driver('bolt://db:7687', neo4j.auth.basic('neo4j', 'matcha'));
 const app    = express();
 
 routes.setRoutes(app);
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use('/api', exprgql( req =>( {
-    schema: schema,
-	graphiql: true,
-	context: { driver },
-	formatError: error => ({
-		message: error.message,
-		//		locations: error.locations,
-		//stack: error.stack ? error.stack.split('\n') : [],
-		//path: error.path,
-	})
-})));
-app.listen(4000, () => console.log("API started on port 4000"));
+
+const apolloServer = new ApolloServer({
+  schema,
+  context: { driver },
+  playground: true
+});
+apolloServer.applyMiddleware({ app });
+
+app.listen(4000, () => console.log("GraphQL API started on localhost:4000/graphql"));
