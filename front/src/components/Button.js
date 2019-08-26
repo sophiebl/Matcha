ab10c7871b587e5095e7ff462a4b41a331682b1e
sophiebl <sboulaao@student.42.fr>
2019-08-26 21:04:10 +0200

    Browse User

--- front/src/components/Button.js
+++ front/src/components/Button.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
-/*props class concatenation*/
-
 const Button = (props) => (
     <a href="" className={props.className}>
         <FontAwesomeIcon className="color-w" size="2x"  icon={['far', 'star']} />
