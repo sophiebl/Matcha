220215fce96b62979572464402c431897aa7319b
aguiot-- <aguiot--@student.42.fr>
2019-08-27 19:24:03 +0200

    Front of /messages/:id (added react-chat-ui)

--- front/src/components/messages/Messages.js
+++ front/src/components/messages/Messages.js
@@ -1,11 +1,76 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from "apollo-boost";
+import { ChatFeed, Message as ChatMessage } from 'react-chat-ui';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Message = () => {
+const Chat = ({ conv }) => {
+  const myid = 5; //TODO: Use real current user id
+
+  const messages = conv.messages.map(({author, content}) => (
+	new ChatMessage({
+	  id: (author.id !== myid) >>> 0,
+	  message: content,
+	})
+  ));
+
   return (
-      <div>
-	    <p>Message</p>
-      </div>
+	<ChatFeed
+	  messages={messages}
+	  isTyping={false}
+	  hasInputField={false}
+	  showSenderName
+	  bubblesCentered={false}
+	  bubbleStyles={
+		{
+		  text: {
+			fontSize: 13
+		  },
+		  chatbubble: {
+			borderRadius: 50,
+			padding: 10
+		  }
+		}
+	  }
+	/>
   );
 }
 
-export default Message;
+const Messages = ({ match }) => {
+  const myid = 5; //TODO: Use real current user id
+
+  const { loading, error, data } = useQuery(gql`
+  {
+	Conversation(id: ${match.params.id}) {
+	  members {
+		id
+		firstname
+	  }
+	  messages(orderBy: id_asc) {
+		id
+		author {
+		  id
+		  firstname
+		}
+		content
+	  }
+	}
+  }
+  `, {fetchPolicy: 'cache-and-network',});
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const members = data.Conversation[0].members.filter(m => (m.id !== myid)).map(m => m.firstname).join(', ');
+
+  return (
+	<div>
+	  <Link to="/messages" style={{color: 'black', display: 'inline-block', float: 'left'}}><FontAwesomeIcon size="3x" icon="angle-left" /></Link>
+	  <p style={{fontSize: '15px', display: 'inline-block'}}><strong>{members}</strong></p>
+	  <Chat conv={data.Conversation[0]}/>
+	</div>
+  );
+}
+
+export default Messages;
