fed48496315ac131400a51de2e171cf9cd64279b
aguiot-- <aguiot--@student.42.fr>
2019-08-29 19:25:09 +0200

    WIP Signup (front side)

--- front/src/components/signup/Signup.js
+++ front/src/components/signup/Signup.js
@@ -1,18 +1,34 @@
-import React/*, { useState }*/ from 'react';
+import React from 'react';
 import Input from '../Input';
+import useForm from 'react-hook-form';
+
+import { gql } from "apollo-boost";
+import { useMutation } from '@apollo/react-hooks';
+
+const SIGNUP = gql`
+  mutation signup($firstname: String!, $email: String!, $password: String!) {
+	signup(firstname: $firstname, email: $email, password: $password)
+  }
+`;
 
 const Signup = () => {
-    return (
-        <form className="signup bg-desc">
-            <Input type="text" placeholder="Nom"/>
-            <Input type="text" placeholder="Prénom"/>
-            <Input type="text" placeholder="Username"/>
-            <Input type="text" placeholder="Email"/>
-            <Input type="password" placeholder="Mot de passe"/>
-            <Input type="password" placeholder="Vérification du mot de passe"/>
-            <button>Sign up</button>
-        </form>
-    );
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => {
+	console.log(data);
+  };
+
+  return (
+	<form className="signup bg-desc" onSubmit={handleSubmit(onSubmit)}>
+	  <Input type="text" name="lastname" placeholder="Nom" reff={register}/>
+	  {errors.lastname && 'Last name is required.'}
+	  <Input type="text" name="firstname" placeholder="Prénom" reff={register}/>
+	  <Input type="text" name="username" placeholder="Username" reff={register}/>
+	  <Input type="text" name="email" placeholder="Email" reff={register}/>
+	  <Input type="password" name="password" placeholder="Mot de passe" reff={register}/>
+	  <Input type="password" name="password-confirmation" placeholder="Vérification du mot de passe" reff={register}/>
+	  <button>Sign up</button>
+	</form>
+  );
 }
 
 export default Signup;
