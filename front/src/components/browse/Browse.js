aeed3f436525fd6c05dbbd3bff77ba1e34eb0f7e
aguiot-- <aguiot--@student.42.fr>
2019-09-04 18:57:32 +0200

    Refactor Browse/InfosContainer

--- front/src/components/browse/Browse.js
+++ front/src/components/browse/Browse.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import InfosContainer from '../userInfo/InfosContainer';
 
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from "apollo-boost";
+
+const GET_USERS = gql`
+{
+    User {
+        uid
+        firstname
+        lastname
+        likesCount
+        prefRadius
+        tags {
+            uid
+            name
+        }
+    }
+}
+`;
+
 const Browse = () => {
+    const { loading, error, data } = useQuery(GET_USERS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error </p>;
+
     return (
         <div className="browse">
-           <InfosContainer /> 
+           { data.User.map((user) => <InfosContainer user={user}/> ) }
         </div>
     );
 }
