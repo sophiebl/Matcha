6bc3f31a96dea86cf386c4bd897d3948d4c22581
aguiot-- <aguiot--@student.42.fr>
2019-10-08 03:37:14 +0200

    Add default values to /preferences

--- front/src/components/profile/Preferences.js
+++ front/src/components/profile/Preferences.js
@@ -7,17 +7,18 @@ import 'rc-slider/assets/index.css';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { gql } from "apollo-boost";
 
-/*
-const GET_INFOS = gql`
-    {
-        me {
-            uid
-            firstname
-            lastname
-        }
-    }
+const ME = gql`
+		{
+			me {
+				uid
+				gender
+				prefAgeMin
+				prefAgeMax
+				prefDistance
+				prefOrientation
+			}
+		}
 `;
-*/
 
 const EDIT_PREFERENCES = gql`
 	mutation UpdateUser($uid: ID!, $gender: String!, $prefAgeMin: Int!, $prefAgeMax: Int!, $prefOrientation: String!, $prefDistance: Int!) {
@@ -34,43 +35,40 @@ const EDIT_PREFERENCES = gql`
 
 
 const Preferences = () => {
-    const wrapperStyle = { width: '80%', margin: 30 };
-    const Handle = Slider.Handle;
-    const distanceHandle = (props) => {
-        const { value, dragging, index, ...restProps } = props;
-        return (
-            <Tooltip
-                prefixCls="rc-slider-tooltip"
-                overlay={`${value} km`}
-                visible={true}
-                placement="bottom"
-                key={index}
-            >
-                <Handle value={value} {...restProps} />
-            </Tooltip>
-        );
-    };
-    const ageHandle = (props) => {
-        const { value, dragging, index, ...restProps } = props;
-        return (
-            <Tooltip
-                prefixCls="rc-slider-tooltip"
-                overlay={`${value} ans`}
-                visible={true}
-                placement="bottom"
-                key={index}
-            >
-                <Handle value={value} {...restProps} />
-            </Tooltip>
-        );
-    };
 
-	const corr = {
-		1: 'homme',
-		2: 'femme',
-		3: 'non-binaire'
+	const wrapperStyle = { width: '80%', margin: 30 };
+	const Handle = Slider.Handle;
+	const distanceHandle = (props) => {
+		const { value, dragging, index, ...restProps } = props;
+		return (
+			<Tooltip
+				prefixCls="rc-slider-tooltip"
+				overlay={`${value} km`}
+				visible={true}
+				placement="bottom"
+				key={index}
+			>
+				<Handle value={value} {...restProps} />
+			</Tooltip>
+		);
 	};
-	const [checkeds, setCheckeds] = useState({ gender: null, orientation: null });
+	const ageHandle = (props) => {
+		const { value, dragging, index, ...restProps } = props;
+		return (
+			<Tooltip
+				prefixCls="rc-slider-tooltip"
+				overlay={`${value} ans`}
+				visible={true}
+				placement="bottom"
+				key={index}
+			>
+				<Handle value={value} {...restProps} />
+			</Tooltip>
+		);
+	};
+
+	const [state, setState] = useState({ first: true, gender: null, prefOrientation: null, prefAgeMin: 18, prefAgeMax: 25, prefDistance: 25});
+
 	const [editPreferences] = useMutation(EDIT_PREFERENCES, {
 		onCompleted: (data) => {
 			console.log("complete " + data);
@@ -81,21 +79,19 @@ const Preferences = () => {
 	});
 
 	const Radio = ({ id, name, label, isSelected, onCheckboxChange }) => (
-        <div>
-            <label>
-                <input
-                    type="radio"
-                    name={name}
-					/*defaultChecked={isSelected}*/
-					checked={checkeds[name] === id}
-					onChange={() => setCheckeds({ ...checkeds, [name]: id })}
+		<div>
+			<label>
+				<input
+					type="radio"
+					name={name}
+					checked={state[name] === label.toLowerCase().replace(' ', '-')}
+					onChange={() => setState({ ...state, [name]: label.toLowerCase().replace(' ', '-') })}
 				/>
 				{label}
-            </label>
-        </div>
-    );
+			</label>
+		</div>
+	);
 
-	const [state, setState] = useState({});
 	const onSliderChange = (value) => {
 		setState({
 			...state,
@@ -109,48 +105,64 @@ const Preferences = () => {
 			prefAgeMax: values[1],
 		});
 	}
-	
-  return (
-    <div className="settings">
-        <h2>Preferences</h2>
 
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-            <div>
-                <h4>Vous etes</h4>
-                <Radio id={1} name="gender" label="Homme">Homme</Radio>
-                <Radio id={2} name="gender" label="Femme">Femme</Radio>
-                <Radio id={3} name="gender" label="Non binaire">Non binaire</Radio>
-            </div>
-            <div style={{width: '15px'}}> </div>
-            <div>
-                <h4>Vous cherchez</h4>
-                <Radio id={4} name="orientation" label="Homme">Homme</Radio>
-                <Radio id={5} name="orientation" label="Femme">Femme</Radio>
-                <Radio id={6} name="orientation" label="Non binaire">Non binaire</Radio>
-            </div>
-        </div>
+	const { loading, error, data } = useQuery(ME);
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error </p>;
 
-        <div style={wrapperStyle}>
-            <p>Distance</p>
-			<Slider min={5} max={200} defaultValue={20} handle={distanceHandle} step={5} onChange={onSliderChange}/>
-        </div>
-        <div style={wrapperStyle}>
-            <p>Age</p>
-            <Range min={18} max={80} defaultValue={[25, 35]} pushable={1} handle={ageHandle} onChange={onRangerChange}/>
-		</div>
+	if (state['first'] === true) {
+		setState({
+			...state,
+			first: false,
+			gender: data.me.gender,
+			prefOrientation: data.me.prefOrientation,
+			prefAgeMin: data.me.prefAgeMin,
+			prefAgeMax: data.me.prefAgeMax,
+			prefDistance: data.me.prefDistance,
+		});
+	}
 
-		<button onClick={() => editPreferences({
-			variables: {
-				uid: "user-fvl84uzk16j2nsa",
-				gender: corr[checkeds['gender']],
-				prefAgeMin: state['prefAgeMin'],
-				prefAgeMax: state['prefAgeMax'],
-				prefOrientation: corr[checkeds['orientation'] - 3],
-				prefDistance: state['prefDistance'],
-			}
-		})}>Enregistrer</button>
-    </div>
-  );
+	return (
+		<div className="settings">
+			<h2>Preferences</h2>
+
+			<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+				<div>
+					<h4>Vous etes</h4>
+					<Radio id={1} name="gender" label="Homme">Homme</Radio>
+					<Radio id={2} name="gender" label="Femme">Femme</Radio>
+					<Radio id={3} name="gender" label="Non binaire">Non binaire</Radio>
+				</div>
+				<div style={{width: '15px'}}> </div>
+				<div>
+					<h4>Vous cherchez</h4>
+					<Radio id={4} name="prefOrientation" label="Homme">Homme</Radio>
+					<Radio id={5} name="prefOrientation" label="Femme">Femme</Radio>
+					<Radio id={6} name="prefOrientation" label="Non binaire">Non binaire</Radio>
+				</div>
+			</div>
+
+			<div style={wrapperStyle}>
+				<p>Distance</p>
+				<Slider min={5} max={200} defaultValue={state['prefDistance']} handle={distanceHandle} step={5} onChange={onSliderChange}/>
+			</div>
+			<div style={wrapperStyle}>
+				<p>Age</p>
+				<Range min={18} max={80} defaultValue={[state['prefAgeMin'], state['prefAgeMax']]} pushable={1} handle={ageHandle} onChange={onRangerChange}/>
+			</div>
+
+			<button onClick={() => editPreferences({
+				variables: {
+					uid: data.me.uid,
+					gender: state['gender'],
+					prefAgeMin: state['prefAgeMin'],
+					prefAgeMax: state['prefAgeMax'],
+					prefOrientation: state['prefOrientation'],
+					prefDistance: state['prefDistance'],
+				}
+			})}>Enregistrer</button>
+			</div>
+	);
 }
 
 export default Preferences;
