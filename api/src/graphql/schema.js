1659572f7d3e53b49732b921f10b36405b896548
sophiebl <sboulaao@student.42.fr>
2019-10-19 11:47:52 +0200

    Confirmation d'inscription par email

--- api/src/graphql/schema.js
+++ api/src/graphql/schema.js
@@ -15,28 +15,97 @@ const resolvers = {
 	Query: {
 		
   },
-  
+
   Mutation: {
 	async signup (_, { firstname, email, username, password }) {
-	  const uid = uniqid('user-');
-	  //const hash = await PBKDF2(password, 'salt', { iterations: 10, hasher: crypto.algo.SHA256, keySize: 256 }).toString();
-	  const hash = await SHA256(password, 'salt').toString();
+		const uid = uniqid('user-');
+		//const hash = await PBKDF2(password, 'salt', { iterations: 10, hasher: crypto.algo.SHA256, keySize: 256 }).toString();
+		const hash = await SHA256(password, 'salt').toString();
+		const emailToken = Math.random() * 10;
+		const url = `http://localhost:3000/verification/${emailToken}/${uid}`;
 
-	  return await session.run(`CREATE (u:User {uid: $uid, firstname: $firstname, username: $username, email: $email, password: $hash}) RETURN u`,
-		{uid, firstname, email, username, hash})
+		const sendmail = require('sendmail')();
+		
+		sendmail({
+			from: 'sophieboulaaouli@gmail.com',
+			to: email,
+			subject: 'test sendmail',
+			html: `Click here to confirm your email : <a href="${url}">${url}</a>`,
+		}, function(err, reply) {
+			console.log(err && err.stack);
+			console.dir(reply);
+		});
+
+		return await session.run(`CREATE (u:User {uid: $uid, firstname: $firstname, username: $username, email: $email, password: $hash, confirmToken: $emailToken}) RETURN u`,
+		{uid, firstname, email, username, hash, emailToken})
 		.then(result => {
-		  	const user = result.records[0].get('u').properties;
-		  	return jwt.sign(
+			const user = result.records[0].get('u').properties;
+			return jwt.sign(
 				{ uid: user.uid },
 				process.env.JWT_SECRET,
 				{ expiresIn: '1y' }
 			)
 		});
 	},
+	async emailverif(_, { uid, confirmToken }) {
+		if (confirmToken != "true")
+		{
+			confirmToken = "true";
+			return await session.run(`MATCH (u:User {uid: $uid}) SET u.confirmToken = 'true' RETURN u`,
+			{uid})
+			.then(result => {
+				const user = result.records[0].get('u').properties;
+				console.log("user");
+				console.log(user);
+				return jwt.sign(
+					{ uid: user.uid },
+					process.env.JWT_SECRET,
+					{ expiresIn: '1y' }
+				)
+			});
+		}
+	}, 
+	async pwdReset(_, { email }) {
+	/*	console.log('email');
+		console.log(email);
+		const uid = uniqid('user-');
+		//const hash = await PBKDF2(password, 'salt', { iterations: 10, hasher: crypto.algo.SHA256, keySize: 256 }).toString();
+//		const hash = await SHA256(password, 'salt').toString();
+		const pwdToken = Math.random() * 10;
+		const url = `http://localhost:3000/verification/${pwdToken}`;
 
+		const sendmail = require('sendmail')();
+		
+		sendmail({
+		  from: 'sophieboulaaouli@gmail.com',
+		  to: email,
+		  subject: 'Forgot password',
+		  html: `Click here to reset your password : <a href="${url}">${url}</a>`,
+		}, function(err, reply) {
+		  console.log(err && err.stack);
+		  console.dir(reply);
+		});*/
+		console.log('before querie');
+
+		return await session.run(`MATCH (u:User {email: $email, confirmToken: 'true'}) RETURN u`,
+		{email})
+		.then(result => {
+			const user = result.records[0].get('u').properties;
+			console.log(user);
+			if (!user) {
+				throw new Error('No user with that email')
+			}
+			return jwt.sign(
+				{ uid: user.uid },
+				process.env.JWT_SECRET,
+				{ expiresIn: '1y' }
+			)
+		});
+	}, 
 	async login (_, { username, password }) {
+		// return await session.run(`MATCH (u:User {username: $username, confirmToken: 'true'}) RETURN u`,
 		const hash = await SHA256(password, 'salt').toString();
-		return await session.run(`MATCH (u:User {username: $username}) RETURN u`,
+		return await session.run(`MATCH (u:User {username: $username) RETURN u`,
 			{username})
 			.then(result => {
 				const user = result.records[0].get('u').properties;
