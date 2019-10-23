25222c329d216e1141630203aec444b77229d910
aguiot-- <aguiot--@student.42.fr>
2019-10-23 21:44:16 +0200

    Add PublicRoute

--- front/src/components/Routes/Routes.js
+++ front/src/components/Routes/Routes.js
@@ -29,14 +29,27 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 };
 */
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => 
-    localStorage.getItem('token') ? (
-        <Component {...props} />
-    ) : (
-        <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )
-    } />
+//TODO: query me to check token
+
+const PublicRoute = ({ component: Component, ...rest }) => (
+	<Route {...rest} render={props => 
+			localStorage.getItem('token') ? (
+				<Redirect to={{ pathname: '/profile', state: { from: props.location } }} />
+			) : (
+				<Component {...props} />
+			)
+	} />
 );
 
-export default PrivateRoute;
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route {...rest} render={props => 
+			localStorage.getItem('token') ? (
+				<Component {...props} />
+			) : (
+				<Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+			)
+	} />
+);
+
+//export default PrivateRoute;
+export { PublicRoute, PrivateRoute };
