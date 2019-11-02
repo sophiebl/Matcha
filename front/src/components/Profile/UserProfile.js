ca32067431e3736a4a5187745292b3937f46519a
aguiot-- <aguiot--@student.42.fr>
2019-11-02 03:54:51 +0100

    Mark profiles as visited when visited

--- front/src/components/Profile/UserProfile.js
+++ front/src/components/Profile/UserProfile.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import LikeDislike from './LikeDislike';
+import { gql } from "apollo-boost";
+import { useMutation } from '@apollo/react-hooks';
+
 import MainInfos from './MainInfos';
 import Bio from './Bio';
+import LikeDislike from './LikeDislike';
 import Tag from './Tag';
 import Nav from "../Nav/Nav";
 import './Profile.scss'
 
+const VISIT_PROFILE = gql`
+	mutation visitProfile($uid: ID!) {
+		visitProfile(uid: $uid) {
+			visitedUsers {
+				username
+			}	
+		}
+	}
+`;
+
 const UserProfile = ({ user, dispatch }) => {
 	const { uid, firstname, lastname, bio, tags, likesCount, prefRadius, avatar } = user;
+
+	const [visitProfile] = useMutation(VISIT_PROFILE, {
+		onError: data => console.log(data),
+	});
+
+	useEffect(() =>	{
+		visitProfile({
+			variables: {
+				uid: uid,
+			}
+		});
+	}, [visitProfile, uid]);
 
 	return (
 		<div>
