f2f9a7826e23773c19526ebfea99093bb43baa66
sophiebl <sophieboulaaouli@gmail.com>
2019-11-20 19:17:48 +0100

    WIP: Upload Image

--- front/src/components/UploadImage/UploadImages.js
+++ front/src/components/UploadImage/UploadImages.js
@@ -5,40 +5,16 @@ import { useQuery, useMutation } from '@apollo/react-hooks';
 import UploadSpinner from './UploadSpinner';
 import Images from './Images';
 import UploadButton from './UploadButton';
-//import uniqid from 'uniqid';
+import nextId from "react-id-generator";
 import { getCurrentUid } from '../../Helpers';
-/*
-const UPLOADIMAGES = gql`
-    mutation uploadImages($file: Upload!) {
-            uploadImages(file: $file) {
-                filename
-        }
-    }
-`;
-*/
-
-const ME_AND_IMAGES = gql`
-		{
-			me {
-				uid
-				gender
-				images {
-					uid
-					url
-				}
-			}
-
-			Image {
-				uid
-				url
-			}
-		}
-`;
 
 const CREATE_IMG = gql`
-	mutation CreateImage($uid: ID!, $url: String!) {
-		CreateImage(uid: $uid, url: $url)
-	}
+    mutation CreateImage($uid: ID!, $url: String!) {
+        CreateImage(uid: $uid, url: $url) {
+            uid
+            url
+        }
+    }
 `;
 
 const ADD_IMG = gql`
@@ -67,37 +43,47 @@ const REMOVE_IMG = gql`
 	}
 `;
 
+const GET_IMGS = gql`
+{
+    Image {
+        uid
+        url
+    }
+}
+`;
+
 
 
 const UploadImages = (avatar) => {
+    const { loading, error, data } = useQuery(GET_IMGS);
+    console.log(data);
     avatar = avatar.location.state.avatar; 
-    // const { loading, error, data } = useQuery(ME_AND_IMAGES);
-	// if (loading) return <p>Loading...</p>;
-	// if (error) return <p>Error </p>;
-
     const [upload, setUpload] = useState(false);
     const [images, setImages] = useState([{avatar}]);
-    // const [user, setUser] = useState({
-	// 	//first: true,
-    //     gender: null,
-    //     images: [],
-	// });
-
     const onError = data => console.log(data);
 
     const [addImg] = useMutation(ADD_IMG, {
         onError,
     });
 
+    const [createImage] = useMutation(CREATE_IMG, {
+        onCompleted: data => {
+            // addImg({
+            //     variables: {
+            //         from: { uid: getCurrentUid() },
+            //         // to: { uid: 'image-1' },
+            //         to: {uid: uidImage}
+            //     }
+            // }
+            //addImg();
+            console.log("Completed", data);
+        },
+	    onError: data => {console.log('Error:', data)},
+    });
+
+
     const onChange = e => {
         // if (e.size < 1000000) {
-            // setUser({
-            //     ...user,
-            //     //first: false,
-            //     gender: data.me.gender,
-            //     images: data.Image,
-            // });
-			//const uidImage = uniqid('image-');
 
             const files = Array.from(e.target.files)
             files.forEach(file => {
@@ -108,10 +94,6 @@ const UploadImages = (avatar) => {
                 formData.append('upload_preset', 'dzhukajo');
                 formData.append('file', file);
                 formData.append('cloud_name', 'dtfunbpou');
-
-                /*files.forEach((file, i) => {
-                    formData.append(i, file)
-                })*/
 
                 fetch(`${process.env.REACT_APP_API_IMG_URL}`, {
                     method: 'POST',
@@ -125,46 +107,47 @@ const UploadImages = (avatar) => {
                         // } else {
                         //     newImgs.push(image);
                         // }
-                        newImgs.push(image);
+                        // newImgs.push(image);
                         console.log("newImages", newImgs)
                         console.log("image en question", image.secure_url)
+                        const uidImage = nextId('image-');
                         setImages(newImgs);
                         setUpload(false);
-		                // const changedImage = images.filter(image => image['url'].toLowerCase() === (added || removed).toLowerCase())[0];
-		                // if (changedImage === undefined)
-                        //  	return;
-                        createImg({
+                        createImage({
                             variables: {
-                                //from: { uid: 'user-7t71gvk2rjpuvf' },
-                                from: { uid: getCurrentUid() },
-                                // to: { uid: changedImage.uid },
-                                 to: { uid: 'image-1' },
-                                //to: {uid: uidImage},
+                                uid: uidImage,
+                                url: image.secure_url,
                             }
-                        }
-                        )
-                        //create img avec id rand et url
-                        //add Img dans OnCompleted
+                        })
                         addImg({
                                 variables: {
-                                    //from: { uid: 'user-7t71gvk2rjpuvf' },
                                     from: { uid: getCurrentUid() },
-                                    // to: { uid: changedImage.uid },
-                                     to: { uid: 'image-1' },
-                                    //to: {uid: uidImage},
+                                    to: { uid: uidImage },
                                 }
                             }
                         )
-                        console.log('OK')
+                        newImgs.push(image);
                         // )
                     })
             });
     // /    }
     }
 
-    const removeImage = id => {
+    const [removeImg] = useMutation(REMOVE_IMG, {
+        onError,
+    });
+
+    const removeImage = (id) => {
         setImages(
             images.filter(image => image.public_id !== id)
+
+            // removeImg({
+            //         variables: {
+            //             from: { uid: getCurrentUid() },
+            //             to: { uid: uidImage },
+            //         }
+            //     }
+            // )
         )
     }
 
