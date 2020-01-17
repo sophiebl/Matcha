3710c3b68fcd82978f433efc24d10202713f6f5d
alexandregv <contact@alexandregv.fr>
2020-01-17 02:56:32 +0100

    Move pubsub/driver/mailstransport in context

--- api/src/graphql/schema.js
+++ api/src/graphql/schema.js
@@ -1,29 +1,12 @@
 import { makeAugmentedSchema } from 'neo4j-graphql-js';
-import { PubSub, withFilter } from 'apollo-server';
+import { withFilter } from 'apollo-server';
 import fs, { exists } from 'fs';
 import jwt from 'jsonwebtoken';
 import uniqid from 'uniqid';
-import nodemailer from 'nodemailer';
 import crypto from 'crypto';
 import dotenv from 'dotenv';
 
 dotenv.config()
-
-import { v1 as neo4j }  from 'neo4j-driver';
-const driver = neo4j.driver('bolt://db', neo4j.auth.basic(process.env.NEO4J_USER || 'neo4j', process.env.NEO4J_PASS || 'letmein'));
-const session = driver.session();
-
-const transporter = nodemailer.createTransport({
-	host: process.env.MAIL_HOST,
-	port: process.env.MAIL_PORT,
-	secureConnection: true,
-	auth: {
-		user: process.env.MAIL_USER,
-		pass: process.env.MAIL_PASS
-	}
-});
-
-const pubsub = new PubSub();
 
 const resolvers = {
 	Query: {
@@ -32,7 +15,7 @@ const resolvers = {
 
 	User: {
 		async elo(obj, args, ctx) {
-			return await session.run(`MATCH (:User)-[r:VISITED|LIKED|DISLIKED|BLOCKED|REPORTED]->(user:User {uid: $uid}) RETURN TYPE(r) AS type, COUNT(r) AS amount ORDER BY amount DESC`, { uid: obj.uid })
+			return await ctx.driver.session().run(`MATCH (:User)-[r:VISITED|LIKED|DISLIKED|BLOCKED|REPORTED]->(user:User {uid: $uid}) RETURN TYPE(r) AS type, COUNT(r) AS amount ORDER BY amount DESC`, { uid: obj.uid })
 				.then(result => {
 					const stats = {};
 					result.records.forEach(record => stats[record.get('type')] = record.get('amount').low);
@@ -60,12 +43,12 @@ const resolvers = {
 				html: `Click here to confirm your email : <a href="${url}">${url}</a>`,
 			};
 
-			transporter.sendMail(mailOptions, (error, info) => {
+			ctx.mailtransport.sendMail(mailOptions, (error, info) => {
 				if (error) console.log(error);
 				else console.log('Email sent: ' + info.response);
 			});
 
-			return await session.run(`CREATE (u:User {uid: $uid, firstname: $firstname, lastname: $lastname, username: $username, email: $email, password: $hash, confirmToken: $confirmToken}) RETURN u`,
+			return await ctx.driver.session().run(`CREATE (u:User {uid: $uid, firstname: $firstname, lastname: $lastname, username: $username, email: $email, password: $hash, confirmToken: $confirmToken}) RETURN u`,
 				{uid, firstname, lastname, username, email, hash, confirmToken})
 				.then(result => {
 					if (result.records.length < 1)
@@ -82,7 +65,7 @@ const resolvers = {
 		async emailVerif(_, { confirmToken }) {
 			if (confirmToken !== 'true')
 			{
-				return await session.run(`MATCH (u:User {confirmToken: $confirmToken}) SET u.confirmToken = 'true' RETURN u`, { confirmToken })
+				return await ctx.driver.session().run(`MATCH (u:User {confirmToken: $confirmToken}) SET u.confirmToken = 'true' RETURN u`, { confirmToken })
 					.then(result => {
 						if (result.records.length < 1)
 							throw new Error('UnknownUser')
@@ -109,7 +92,7 @@ const resolvers = {
 				else console.log('Email sent: ' + info.response);
 			});
 
-			return await session.run(`MATCH (u:User {email: $email}) SET u.resetToken = $resetToken RETURN u`, { email, resetToken })
+			return await ctx.driver.session().run(`MATCH (u:User {email: $email}) SET u.resetToken = $resetToken RETURN u`, { email, resetToken })
 				.then(result => {
 					if (result.records.length < 1)
 						throw new Error('UnknownUser')
@@ -120,7 +103,7 @@ const resolvers = {
 
 		async resetPassword(_, { password, resetToken }) {
 			const hash = crypto.createHmac('sha256', 'matcha').update(password + 'salt').digest('hex');
-			return await session.run(`MATCH (u:User {resetToken: $resetToken}) SET u.password = $hash RETURN u`, { resetToken, hash })
+			return await ctx.driver.session().run(`MATCH (u:User {resetToken: $resetToken}) SET u.password = $hash RETURN u`, { resetToken, hash })
 				.then(result => {
 					if (result.records.length < 1)
 						throw new Error('UnknownUser')
@@ -129,9 +112,9 @@ const resolvers = {
 				});
 		},
 
-		async login (_, { username, password }) {
+		async login (_, { username, password }, ctx) {
 			const hash = crypto.createHmac('sha256', 'matcha').update(password + 'salt').digest('hex');
-			return await session.run(`MATCH (u:User) WHERE toLower(u.username) = toLower($username) RETURN u`, { username })
+			return await ctx.driver.session().run(`MATCH (u:User) WHERE toLower(u.username) = toLower($username) RETURN u`, { username })
 				.then(result => {
 					if (result.records.length < 1)
 						throw new Error('UnknownUsername');
@@ -142,19 +125,19 @@ const resolvers = {
 						throw new Error('EmailNotConfirmed');
 					if (user.banned == true)
 						throw new Error('UserBanned');
-					pubsub.publish('USER_CONNECTED', user);
+					ctx.pubsub.publish('USER_CONNECTED', user);
 					return jwt.sign({ uid: user.uid }, process.env.JWT_SECRET, { expiresIn: '1y' });
 				});	
 		},
 
 		async reportUser(_, { uid }, ctx) {
-			return await session.run(`MATCH (me:User {uid: $meUid}), (target:User {uid: $uid}) MERGE (me)-[:REPORTED]->(target) WITH me, target MATCH (:User)-[r:REPORTED]->(target) RETURN COUNT(r) as reportsCount`, { meUid: ctx.cypherParams.currentUserUid, uid })
+			return await ctx.driver.session().run(`MATCH (me:User {uid: $meUid}), (target:User {uid: $uid}) MERGE (me)-[:REPORTED]->(target) WITH me, target MATCH (:User)-[r:REPORTED]->(target) RETURN COUNT(r) as reportsCount`, { meUid: ctx.cypherParams.currentUserUid, uid })
 				.then(async result => {
 					if (result.records.length < 1)
 						throw new Error('UnknownUser')
 					const reportsCount = result.records[0].get('reportsCount').low;
 					if (reportsCount >= 5)
-						await session.run(`MATCH (target:User {uid: $uid}) SET target.banned = true`, { uid })
+						await ctx.driver.session().run(`MATCH (target:User {uid: $uid}) SET target.banned = true`, { uid })
 					return reportsCount;
 				});	
 		},
@@ -164,7 +147,7 @@ const resolvers = {
 	Subscription: {
 		userConnected: {
 			subscribe: withFilter(
-				() => pubsub.asyncIterator('USER_CONNECTED'),
+				(_, variables, context) => context.pubsub.asyncIterator('USER_CONNECTED'),
 				(payload, variables) => payload.uid === variables.uid,
 			),
 			resolve: (user) => user,
--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,8 @@
 import schema  from './graphql/schema.js';
 import { v1 as neo4j }  from 'neo4j-driver';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, PubSub } from 'apollo-server';
 import jwt from 'jsonwebtoken';
+import nodemailer from 'nodemailer';
 import uniqid from 'uniqid';
 import dotenv from 'dotenv'
 
@@ -14,12 +15,26 @@ var corsOptions = {
 	credentials: true
 };
 
+const mailtransport = nodemailer.createTransport({
+	host: process.env.MAIL_HOST,
+	port: process.env.MAIL_PORT,
+	secureConnection: true,
+	auth: {
+		user: process.env.MAIL_USER,
+		pass: process.env.MAIL_PASS
+	}
+});
+
+const pubsub = new PubSub();
+
 const apolloServer = new ApolloServer({
 	schema,
 	cors: corsOptions,
-	context: ({ req, connection }) => connection ? (connection.context) : ({
+	context: ({ req, connection }) => connection ? ({...connection.context, pubsub}) : ({
 		driver,
+		pubsub,
 		req,
+		mailtransport,
 		headers: req.headers,
 		token: (req.headers.authorization && req.headers.authorization.slice(7)) || '',
 		cypherParams: {
@@ -29,12 +44,11 @@ const apolloServer = new ApolloServer({
 				return decoded.uid;
 			}),
 			uniqid: uniqid(),
-		}
+		},
 	}),
 	subscriptions: {
 		onConnect: (connectionParams, webSocket, context) => {
-			console.log(`Subscription client connected using Apollo server's built-in SubscriptionServer.`);
-			console.log(connectionParams);
+			console.log(`Subscription client connected.`);
 			return {
 				headers: {
 					Authorization: `Bearer ${connectionParams.token}`,
